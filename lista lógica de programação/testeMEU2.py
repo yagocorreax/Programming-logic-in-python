b463b7dc4f719e4d1bf8b6045119afe2abdbdf07
Yago Ferreira <yagocmferreira9@gmail.com>
2025-12-05 12:07:12 -0300

    Conversão de tempo criado por mim
    Neste exercício imaginei uma situação em que precisasse converter o tempo em uma unidade específica e retornasse o mesmo em todas as outras.

---
+++ lista lógica de programação/testeMEU2.py
@@ -0,0 +1,8 @@
+horas = int(input("Digite um valor em horas:"))
+minutos = horas * 60
+segundos = horas * 3600
+milisegundos = horas * 3600 * 1000
+print(f"{horas} horinhas correspondem a:")
+print(f"são {minutos} minutos")
+print(f"são {segundos} segundos")
+print(f"são {milisegundos} milisegundos")
