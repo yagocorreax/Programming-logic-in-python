62bd42fb30dfa3f8a2aa78df38f003f75ccee41a
Yago Ferreira <yagocmferreira9@gmail.com>
2025-12-05 12:09:22 -0300

    Conversão de moeda criado por mim
    Neste exercício, imaginei uma conversão de reais para euro, ou seja, quantos euros você conseguiria comprar com uma determinada quantia em reais.

---
+++ lista lógica de programação/testeMEU3.py
@@ -0,0 +1,3 @@
+Reais = float(input("Boa noite, senhor! gostaria de saber a quantia em R$:"))
+Euro = Reais / 6,15
+print(f"Com essa quantia em R$ {Reais}, o senhor consegue comprar € {Euro} euros.")
