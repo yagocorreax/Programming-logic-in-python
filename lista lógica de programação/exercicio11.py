4a1344602d9b4c1db6d7dc5623dde18e3048f145
Yago Ferreira <yagocmferreira9@gmail.com>
2025-12-05 13:34:03 -0300

    Cálculo do Delta
    Neste exercício foi proposto o cálculo do delta de uma determinada equação do segundo grau.

---
+++ lista lógica de programação/exercicio11.py
@@ -0,0 +1,5 @@
+A = int(input("Digite o valor de A:"))
+B = int(input("Digite o valor de B:"))
+C = int(input("Digite o valor de C:"))
+Delta = B ** 2  - 4 * A * C
+print(f" O valor de delta é: {Delta}")
