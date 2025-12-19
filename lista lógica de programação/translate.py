1cedc29120d285fbf4291703b4fa51d0eb013145
Yago Ferreira <yagocmferreira9@gmail.com>
2025-12-19 15:38:19 -0300

    Function translate
    Nesse caso criei uma função que traduz outro idioma.

---
+++ lista lógica de programação/translate.py
@@ -0,0 +1,8 @@
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+print(translate(input("Enter a phrase: ")))
