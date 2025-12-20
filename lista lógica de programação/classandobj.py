ab56067948754bbcd284b25028d633c5eb626c90
Yago Ferreira <yagocmferreira9@gmail.com>
2025-12-19 23:33:56 -0300

    Create classandobj.py

---
+++ lista lógica de programação/classandobj.py
@@ -0,0 +1,6 @@
+class student: #classe
+    def __init__(self, name, gpa, major, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
