ceac3d12762ca6fc63568de10d2dfffe9b2792a3
Yago Ferreira <yagocmferreira9@gmail.com>
2025-12-19 23:34:07 -0300

    Create importclass.py

---
+++ lista lógica de programação/importclass.py
@@ -0,0 +1,6 @@
+from classandobj import student
+student1 = student("Anna luiza", "enfermagem", 5.0, False)
+print(student1.name)
+print(student1.major)
+print(student1.gpa)
+print(student1.is_on_probation)
